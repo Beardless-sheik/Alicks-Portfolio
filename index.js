95d043680c207d6095b400569cb6b43fac151358
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-06-29 12:07:56 +0200

    Update index.js
    Remove comments from index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function popUpMenu() {
   document.querySelector('#mobile-container').classList.toggle('hide-mobile-content');
   document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
-  //  document.querySelector('header').classList.toggle()
 }
 
 function removeMenu() {
   document.querySelector('#mobile-container').classList.toggle('hide-mobile-menu');
   document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
-  //  ['mobile-backgrond-image-1','mobile-backgrond-image-2'].map( className =>
-  //  document.querySelector('#image2').classList.toggle(className));
 }
 const hamburger = document.querySelector('.header-icon-nav');
 hamburger.addEventListener('click', popUpMenu);
