086f46cfdcff273ed5f0d018b61a993d46c7557e
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-06-29 11:12:16 +0200

    Move onclick items from index.html to index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-function popUpMenu () {
-    document.querySelector("#mobile-container").classList.toggle("hide-mobile-content");
-    document.querySelector("#mobile-menu").classList.toggle("show-mobile-menu");
-    //document.querySelector('header').classList.toggle()
+function popUpMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-mobile-content');
+  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
+  //  document.querySelector('header').classList.toggle()
 }
 
-function removeMenu () { 
-    document.querySelector("#mobile-container").classList.toggle("hide-mobile-menu");
-    document.querySelector("#mobile-menu").classList.toggle("show-mobile-menu");
-    // ["mobile-backgrond-image-1","mobile-backgrond-image-2"].map( className => 
-    //     document.querySelector("#image2").classList.toggle(className));
+function removeMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-mobile-menu');
+  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
+  //  ['mobile-backgrond-image-1','mobile-backgrond-image-2'].map( className =>
+  //  document.querySelector('#image2').classList.toggle(className));
 }
+const hamburger = document.querySelector('.header-icon-nav');
+hamburger.addEventListener('click', popUpMenu);
+const closeIcon = document.querySelector('.remove-menu');
+closeIcon.addEventListener('click', removeMenu);
