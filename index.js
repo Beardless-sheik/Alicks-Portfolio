b6cc542849ad367d99b01aa6bab513321ba3ff71
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-06-30 13:29:18 +0200

    Update index.js
    Create pop up of project dynamically using static information in HTML

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function popUpMenu() {
-  document.querySelector('#mobile-container').classList.toggle('hide-mobile-content');
+function popUpMobileMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
   document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
 }
 
-function removeMenu() {
-  document.querySelector('#mobile-container').classList.toggle('hide-mobile-menu');
+function removeMobileMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
   document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
 }
+
+function popUpProject() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#project').classList.toggle('hide-project');
+}
+
+function removeProject() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#project').classList.toggle('hide-project');
+}
+// Implementation of Including Mobile Menu & Closing it
 const hamburger = document.querySelector('.header-icon-nav');
-hamburger.addEventListener('click', popUpMenu);
+hamburger.addEventListener('click', popUpMobileMenu);
 const closeIcon = document.querySelector('.remove-menu');
-closeIcon.addEventListener('click', removeMenu);
+closeIcon.addEventListener('click', removeMobileMenu);
+
+// Implementation of PopUp Window and Closing it
+const seeProjectButton = document.querySelector('.see-project-text');
+seeProjectButton.addEventListener('click', popUpProject);
+const popUpClosIcon = document.querySelector('close-icon-pop-up');
+popUpClosIcon.addEventListener('click', removeProject);
+
+
+
