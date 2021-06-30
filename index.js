224dd411ac41ac1c6e5f663cd164c8648e8b3a4c
Beardless_sheik <44676055+Beardless-sheik@users.noreply.github.com>
2021-06-30 14:34:52 +0200

    Update index.js
    Added project data in an object

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ function removeProject() {
   document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
   document.querySelector('#project').classList.toggle('hide-project');
 }
+
+const projectData = [{
+  key:'pro1',
+  title:'Multi Post stories - 1',
+  imageSrc:`./images/pop-up-menu-image.jpeg',
+  projectTextDescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+  is simply dummy text of the printing and typesetting  ever since the 1500s, 
+  when an unknown printer took a galley of type veris lapoa todoe`,
+  technoogies:[ 'Javascript','Ruby','HTML','CSS'],
+  links:[
+    {
+      liveIcon:'./images/icons/IconExport.svg',
+      liveLink:'https://github.com/Beardless-sheik'
+    },
+    {
+      sourceIcon:'./images/icons/IconGitHub.svg',
+      sourceLink:'https://github.com/Beardless-sheik'
+    }
+  ]
+}
+]
 // Implementation of Including Mobile Menu & Closing it
 const hamburger = document.querySelector('.header-icon-nav');
 hamburger.addEventListener('click', popUpMobileMenu);
