f4361d3aaf0a2a478f36d50705aab73d3bc828c5
Caleb Anderson <buyananderson@gmail.com>
2021-07-01 11:37:46 +0200

    Add the popup window logic dynamicaly in my index.js file

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-function popUpMobileMenu() {
-  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
-  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
-}
-
-function removeMobileMenu() {
-  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
-  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
-}
-
-function popUpProject() {
-  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
-  document.querySelector('#project').classList.toggle('hide-project');
-}
-
-function removeProject() {
-  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
-  document.querySelector('#project').classList.toggle('hide-project');
-}
-
 const projectData = [{
-  key:'pro1',
+  key:'project1',
   title:'Multi Post stories - 1',
-  imageSrc:`./images/pop-up-menu-image.jpeg',
-  projectTextDescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  imageSrc:'/images/pop-up-menu-image.jpeg',
+  projectTextDescription:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
   is simply dummy text of the printing and typesetting  ever since the 1500s, 
@@ -38,19 +18,139 @@ const projectData = [{
       sourceLink:'https://github.com/Beardless-sheik'
     }
   ]
-}
+  },
+  {
+    key:'project2',
+    title:'Multi Post stories - 2',
+    imageSrc:'./images/pop-up-menu-image.jpeg',
+    projectTextDescription:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+    is simply dummy text of the printing and typesetting  ever since the 1500s, 
+    when an unknown printer took a galley of type veris lapoa todoe`,
+    technoogies:[ 'Javascript','Express','HTML','SQL'],
+    links:[
+      {
+        liveIcon:'./images/icons/IconExport.svg',
+        liveLink:'https://github.com/Beardless-sheik'
+      },
+      {
+        sourceIcon:'./images/icons/IconGitHub.svg',
+        sourceLink:'https://github.com/Beardless-sheik'
+      }]
+    },
+    {
+      key:'project3',
+      title:'Multi Post stories - 3',
+      imageSrc:'./images/pop-up-menu-image.jpeg',
+      projectTextDescription:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+      is simply dummy text of the printing and typesetting  ever since the 1500s, 
+      when an unknown printer took a galley of type veris lapoa todoe`,
+      technoogies:[ 'Javascript','Ruby','HTML','CSS'],
+      links:[
+        {
+          liveIcon:"./images/icons/IconExport.svg",
+          liveLink:'https://github.com/Beardless-sheik'
+        },
+        {
+          sourceIcon:"./images/icons/IcfgonGitHub.svg",
+          sourceLink:'https://github.com/Beardless-sheik'
+        }
+      ]
+      },
+      {
+        key:'project4',
+        title:'Multi Post stories - 4',
+        imageSrc:'./images/pop-up-menu-image.jpeg',
+        projectTextDescription:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+        is simply dummy text of the printing and typesetting  ever since the 1500s, 
+        when an unknown printer took a galley of type veris lapoa todoe`,
+        technoogies:[ 'Javascript','Ruby','HTML','CSS'],
+        links:[
+          {
+            liveIcon:'./images/icons/IconExport.svg',
+            liveLink:'https://github.com/Beardless-sheik'
+          },
+          {
+            sourceIcon:'./images/icons/IconGitHub.svg',
+            sourceLink:'https://github.com/Beardless-sheik'
+          }
+        ]
+        } 
 ]
+
+function popUpMobileMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
+}
+
+function removeMobileMenu() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#mobile-menu').classList.toggle('show-mobile-menu');
+}
+
+function popUpProject() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#project').classList.toggle('hide-project');
+  implementMobilePopUp(this.id);
+}
+
+function removeProject() {
+  document.querySelector('#mobile-container').classList.toggle('hide-main-mobile-content');
+  document.querySelector('#project').classList.toggle('hide-project');
+}
+
+function implementMobilePopUp(projectId) {
+  const headerTitle = document.querySelector('.pop-up-header-text');
+  const projectImage = document.querySelector('.pop-up-image');
+  const projectDescription = document.querySelector('.pop-up-text-description');
+  const projectSkills = document.querySelector('.pop-up-skill-item-container');
+  const projectButtons = document.querySelector('.pop-up-button-container');
+  projectData.forEach ((project) => {
+    if(projectId===project.key){
+      headerTitle.textContent = project.title;
+      projectImage.src = project.imageSrc;
+      projectDescription.textContent = project.projectTextDescription;
+      project.technoogies.forEach( technologies => {
+      projectSkills.innerHTML += `<li>
+        <p class="skill-item-language pop-up-skill-border">${technologies}</p>
+      </li>`
+      });
+      projectButtons.innerHTML =`
+      <li class="pop-up-button-background">
+        <a href="${project.links[0].liveLink}" class="pop-up-button-text">
+          <p>See Live</p> <img src="${project.links[0].liveIcon}" alt="">
+        </a>
+      </li>
+      <li class="pop-up-button-background-2">
+        <a href="${project.links[1].sourceLink}" class="pop-up-button-text">
+          <p>See Source</p> <img src="${project.links[1].sourceIcon}" alt="">
+        </a>
+      </li>`; 
+    }
+  });
+}
+
+
 // Implementation of Including Mobile Menu & Closing it
 const hamburger = document.querySelector('.header-icon-nav');
 hamburger.addEventListener('click', popUpMobileMenu);
 const closeIcon = document.querySelector('.remove-menu');
 closeIcon.addEventListener('click', removeMobileMenu);
 
-// Implementation of PopUp Window and Closing it
-const seeProjectButton = document.querySelector('.see-project-text');
-seeProjectButton.addEventListener('click', popUpProject);
-const popUpClosIcon = document.querySelector('close-icon-pop-up');
-popUpClosIcon.addEventListener('click', removeProject);
+//Implementation of Events on Project Buttons
+const projectButton1 = document.querySelector('#project1');
+projectButton1.addEventListener('click', popUpProject);
+const projectButton2 = document.querySelector('#project2');
+projectButton2.addEventListener('click', popUpProject);
+const projectButton3 = document.querySelector('#project3');
+projectButton3.addEventListener('click', popUpProject);
+const projectButton4 = document.querySelector('#project4');
+projectButton4.addEventListener('click', popUpProject);
 
 
 
